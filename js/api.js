056b03f55b14edceaa4046353bb51bda97f7828b
Kaartik Singh <kartik_singh@live.in>
2015-05-01 04:20:36 +0530

    imdb ratings added

--- js/api.js
+++ js/api.js
@@ -6,27 +6,39 @@ $(document).ready(function(){
 
 function getimdbinfo(title)
 {
+  var str;
   $.ajax({
-    url: "http://www.omdbapi.com/?s=" + title,
-    dataType:"json",
-    success:function(data) {
-      console.log(data);
-      var str='<table class="table">';
-       str += "<thead style='color:#F9D732'><th>Index</th><th>Title</th><th>Type</th><th>Year</th><th>imdbID</th></thead>";
-       $.each(data.Search,function(index,element){
-        str += "<tr class='info'>";
-                    str += "<td>" + index + "</td>";
-                    str += "<td>" + element.Title + "</td>";
-                    str += "<td>" + element.Type + "</td>";
-                    str += "<td>" + element.Year + "</td>";
-                    str += "<td>" + element.imdbID + "</td>";
-                    str += "</tr>";
-                });
-       str += '</table>';
-
-                // insert the html
-                $("#results").html(str);
+    url: "http://www.omdbapi.com/?s=" + title+"&r="+"json",
+    success:function(data)
+    {
+      
+      
+      $.each(data.Search,function(index,element){
+                                                          i=0;
+                                                          str='<table class="table">';
+                                                          str += "<thead style='color:#F9D732'><th>Index</th><th>Title</th><th>Type</th><th>Year</th><th>imdbID</th><th>imdbRating</th></thead>";
+                                                  $.ajax({
+                                                          url: "http://www.omdbapi.com/?t=" + element.Title+"&r="+"json",
+                                                          success:function(rate) 
+                                                          {
+                                                            
+                                                            str += "<tr class='info'>";
+                                                            str += "<td>" + ++i + "</td>";
+                                                            str += "<td>" + rate.Title + "</td>";
+                                                            str += "<td>" + rate.Type + "</td>";
+                                                            str += "<td>" + rate.Year + "</td>";
+                                                            str += "<td>" + rate.imdbID + "</td>";
+                                                            str += "<td>" + rate.imdbRating + "</td>";  
+                                                            
+                                                            str += "</tr>";
+                                                             console.log(str);
+                                                            $("#results").html(str);
        
-    }
+                                                          }
+                                                        })
+                                                });
+       
+      
+  }
   })
 }
